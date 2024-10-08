ee2f67ae71aa38dee1c521daaa7e4a656b985942
shreya227 <shreyanadkarni95105@gmail.com>
2024-10-08 18:32:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'WasteDetection',
+    version= '0.0.0',
+    author= 'Shreya Nadkarni',
+    author_email= 'manunadkarni0@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
